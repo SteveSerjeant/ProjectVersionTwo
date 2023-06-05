613df42f85a2c10591a506b99ce29df1d91bcd1d
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2023-06-05 16:18:01 +0100

    initial set up of login form

--- src/index.php
+++ src/index.php
@@ -11,15 +11,42 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--    stylesheet for the username and password icons-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+    <!--bootstrap css for alerts-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+
     <link rel="stylesheet" href="../css/stylesheet.css" type="text/css">
+    <link rel="stylesheet" href="../css/loginForm.css" type="text/css">
 </head>
 <body>
 
+<div class="login">
+    <h1>Dashboard Login</h1>
+    <form action="authenticate.php" method="post">
+        <label for="username">
+        <i class="fas fa-user"></i>
+        </label>
+        <input type="text" name="username" placeholder="Username" id="username" required>
 
-<p>
+        <label for="password">
+        <i class="fas fa-lock"></i>
+        </label>
+        <input type="password" name="password" placeholder="Password" id="password" required>
+        <input type="submit" name="btnLogin" value="Submit">
+        <p>Not a User already? <a href="register.php">Sign Up Now</a>.</p>
 
-    Hello dev branch
-</p>
+
+    </form>
+</div>
+
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+<!--<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>-->
+<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>-->
+<!---->
+<!--<script src="../javascript/for-alerts.js"></script>-->
 
 </body>
 
