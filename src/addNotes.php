dcd9ca1102bcbb5fc3abc851698d0f2d1ef070be
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2023-06-13 19:43:41 +0100

    Start of scan on demand and saveScans

--- src/addNotes.php
+++ src/addNotes.php
@@ -6,20 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-
-    <!--    <link rel="stylesheet" href="../css/popup.css" type="text/css">-->
-<!--    <link rel="stylesheet" href="../css/stylesheet.css" type="text/css">-->
-<!--    <link rel="stylesheet" href="../css/forNavbar.css" type="text/css">-->
-
-    <!-- CSS only -->
-<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">-->
-<!--    -->
-    <!-- JavaScript Bundle with Popper -->
-<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>-->
-
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
-
     <style type="text/css">
         .wrapper {
             width: 300px;
@@ -45,27 +31,6 @@ $ip = $_GET['id'];
 
 ?>
 
-<!--<div class="wrapper">-->
-<!--    <div class="container-fluid" style="background-color: green; margin-top: 30px">-->
-<!---->
-<!--        <div class="row">-->
-<!--            <div class="col-md-12">-->
-<!--                <h2 id="noteTitle">Add Notes</h2>-->
-<!--            </div>-->
-<!--            <form action="saveNotes.php" method="post">-->
-<!--                <div class="form-group">-->
-<!--                    <label>Enter Notes</label>-->
-<!--                    <input type="text" name="toSave" id="toSave" class="form-control">-->
-<!--                </div>-->
-<!--                <input type="hidden" name="id" value=--><?php //echo $_GET['id'];?><!-->
-<!--                <input type="submit" class="btn btn-primary" name="submit" value="Save">-->
-<!---->
-<!--            </form>-->
-<!---->
-<!--        </div>-->
-<!---->
-<!--    </div>-->
-<!--</div>-->
 
 
 </body>
